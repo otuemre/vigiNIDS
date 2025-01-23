56e80ddcf6ec5fc247d67b87594bd857ceb59934
EmreOTU <dev.emre17@gmail.com>
2025-01-23 23:09:44 +0000

    Install libraries and add version control file

---
+++ version_val.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib
+import seaborn as sns
+import sklearn
+import notebook
+
+# Print package versions
+print("NumPy version:", np.__version__)
+print("Pandas version:", pd.__version__)
+print("Matplotlib version:", matplotlib.__version__)
+print("Seaborn version:", sns.__version__)
+print("Scikit-learn version:", sklearn.__version__)
+print("Jupyter Notebook version:", notebook.__version__)
